6b9db56951a4ebce4b62a75f481e4a9f5424a62e
Ivan Jandrić <ijandric97@gmail.com>
2020-08-19 07:01:21 +0200

    Added some basic mobile scaling for container grid

--- src/components/GamePage/ContainerGrid/ContainerGrid.tsx
+++ src/components/GamePage/ContainerGrid/ContainerGrid.tsx
@@ -1,20 +1,69 @@
-import React from 'react';
+import React, { CSSProperties, useRef } from 'react';
 import { useGlobalState } from '../../../state/GlobalState';
+import { Problem } from '../../../state/reducers/ProblemReducer';
 
 import Container from '../Container/Container';
 
 import './ContainerGrid.css';
+import { breaks } from '../../../util/misc';
 
-const ContainerGrid: React.FC<any> = () => {
+type ContainerGridProps = {
+  height: number;
+  width: number;
+};
+
+const ContainerGrid: React.FC<ContainerGridProps> = ({ height, width }) => {
+  const constraintsRef = useRef(null);
   const {
-    state: { problem }
+    state: { client, problem }
   } = useGlobalState();
 
-  console.log(problem);
+  let left = client.width / 2 - width * (2 / 3); // DESKTOP
+  if (client.width < breaks.sm) {
+    left = client.width / 2 - width / 2; // MOBILE
+  }
+
+  const ContainerGridStyle: CSSProperties = {
+    width: `${width}px`,
+    height: `${height}px`,
+    left: `${left}px`
+  };
+
+  const renderContainers = ({ col_size, row_size, current, data }: Problem) => {
+    const conH = height / (col_size + 2);
+    const conW = width / row_size;
+
+    let containers: JSX.Element[] = [];
+
+    for (let i = 0; i < data.length; i++) {
+      for (let j = 0; j < data[i].length; j++) {
+        const el = data[i][j];
+        const bottom = (data[i].length - 1 - j) * conH;
+
+        if (j === 0) {
+          // TODO: MAKE THEM DRAGGABLE
+          console.log(data[i][j]);
+        }
+        containers.push(
+          <Container
+            key={`${i}.${j}`}
+            width={conW}
+            height={conH}
+            left={i * conW}
+            bottom={bottom}
+            next={el.value === current}
+            number={el.value}
+          />
+        );
+      }
+    }
+
+    return containers;
+  };
 
   return (
-    <div className="containers">
-      <Container />
+    <div className="containers" style={ContainerGridStyle} ref={constraintsRef}>
+      {renderContainers(problem as Problem)}
     </div>
   );
 };
